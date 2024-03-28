a0ab9fe8777fb1773ab7894267352446caf47f25
John Harvardalinayu03 <alinayu@college.harvard.edu>
2024-03-27 20:35:52 -0400

    strassen

--- strassen.py
+++ strassen.py
@@ -1,40 +1,48 @@
-def read_matrix(filename, d):
-    with open(filename, 'r') as f:
-        numbers = [int(line.strip()) for line in f]
-    A = [numbers[i:i+d] for i in range(0, d**2, d)]
-    B = [numbers[i:i+d] for i in range(d**2, 2*d**2, d)]
+import numpy as np
+
+def read_matrix(filename, dimension):
+    """Read matrices from a file."""
+    data = np.loadtxt(filename, dtype=int)
+    A = data[:dimension**2].reshape(dimension, dimension)
+    B = data[dimension**2:].reshape(dimension, dimension)
     return A, B
 
 def standard_matrix_multiplication(A, B):
-    n = len(A)
-    C = [[0 for _ in range(n)] for _ in range(n)]
+    """Perform standard matrix multiplication."""
+    n = A.shape[0]
+    C = np.zeros((n, n), dtype=int)
     for i in range(n):
         for j in range(n):
             for k in range(n):
-                C[i][j] += A[i][k] * B[k][j]
+                C[i, j] += A[i, k] * B[k, j]
     return C
 
 def add_matrices(A, B):
-    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+    """Add two matrices."""
+    return A + B
 
 def subtract_matrices(A, B):
-    return [[A[i][j] - B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+    """Subtract matrix B from A."""
+    return A - B
 
-def get_matrix_quadrants(matrix):
-    n = len(matrix)
-    mid = n // 2
-    A = [[matrix[i][j] for j in range(mid)] for i in range(mid)]
-    B = [[matrix[i][j] for j in range(mid, n)] for i in range(mid)]
-    C = [[matrix[i][j] for j in range(mid)] for i in range(mid, n)]
-    D = [[matrix[i][j] for j in range(mid, n)] for i in range(mid, n)]
-    return A, B, C, D
+def split_matrix(A):
+    """Split matrix into quadrants."""
+    row, col = A.shape
+    row2, col2 = row // 2, col // 2
+    return A[:row2, :col2], A[:row2, col2:], A[row2:, :col2], A[row2:, col2:]
 
 def strassen(A, B, n0):
-    n = len(A)
+    """Strassen's algorithm with crossover to the standard algorithm."""
+    n = A.shape[0]
     if n <= n0:
         return standard_matrix_multiplication(A, B)
-    A11, A12, A21, A22 = get_matrix_quadrants(A)
-    B11, B12, B21, B22 = get_matrix_quadrants(B)
+    if n % 2 != 0:
+        A = np.pad(A, [(0, 1), (0, 1)], mode='constant', constant_values=0)
+        B = np.pad(B, [(0, 1), (0, 1)], mode='constant', constant_values=0)
+        n += 1
+    
+    A11, A12, A21, A22 = split_matrix(A)
+    B11, B12, B21, B22 = split_matrix(B)
     
     P1 = strassen(add_matrices(A11, A22), add_matrices(B11, B22), n0)
     P2 = strassen(add_matrices(A21, A22), B11, n0)
@@ -49,24 +57,20 @@ def strassen(A, B, n0):
     C21 = add_matrices(P2, P4)
     C22 = add_matrices(subtract_matrices(add_matrices(P1, P3), P2), P6)
 
-    C = [[0 for _ in range(n)] for _ in range(n)]
-    for i in range(n // 2):
-        for j in range(n // 2):
-            C[i][j] = C11[i][j]
-            C[i][j + n // 2] = C12[i][j]
-            C[i + n // 2][j] = C21[i][j]
-            C[i + n // 2][j + n // 2] = C22[i][j]
-    return C
+    C = np.vstack((np.hstack((C11, C12)), np.hstack((C21, C22))))
+    if C.shape[0] != n:
+        C = C[:n, :n]
+    return C[:dimension, :dimension]
 
-def main(flag, dimension, inputfile):
-    A, B = read_matrix(inputfile, dimension)
-    n0 = 15 if dimension % 2 == 0 else 37  
-    C = strassen(A, B, n0)
-    for i in range(dimension):
-        print(C[i][i])
+def print_diagonal(C):
+    """Print the diagonal of the matrix."""
+    for i in range(C.shape[0]):
+        print(C[i, i])
 
 if __name__ == "__main__":
     import sys
     flag, dimension, inputfile = sys.argv[1], int(sys.argv[2]), sys.argv[3]
-    main(flag, int(dimension), inputfile)
-
+    A, B = read_matrix(inputfile, dimension)
+    n0 = 15 if dimension % 2 == 0 else 37
+    C = strassen(A, B, n0)
+    print_diagonal(C)
